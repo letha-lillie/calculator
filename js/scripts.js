a2e1eae561da2e58d36102e73cfcc32628b621d2
Katy Roberts <letha.lillie@gmail.com>
2018-09-05 10:31:19 -0700

    New calculator project first commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var add = function(number1, number2){
+return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Please enter a number:"));
+
+var number2 = parseInt(prompt("Please just one more number:"));
+
+alert(add(number1,number2));
